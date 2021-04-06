5af858bca2bf1bf126dc07593a49966315f759ff
Aboud Chamoun <aboud.chamoun@yahoo.com>
2021-04-06 17:47:35 +0200

    implementing a method and its tests for the logarithmic complexity

--- src/main/TimeComplexity.java
+++ src/main/TimeComplexity.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 /**
  * 
  * @author aboud
@@ -22,23 +24,58 @@ public class TimeComplexity {
 	 * 
 	 * That means the proportion between the size of the input "n" and the number of
 	 * executions of the statement is linear. that means if "n" gets bigger by 1,
-	 * the number of executions of the statement "arr[i] == number" gets bigger also
-	 * by 1.
+	 * the number of executions of the statement "if(arr[i] == number)" gets bigger
+	 * also by 1.
 	 * 
 	 * @param arr:    the array to find the number in
 	 * @param number: the number to be found
-	 * @return: number of the execution of the statement "arr[i] == number"
+	 * @return: number of the execution of the statement "if(arr[i] == number)"
 	 */
-	public int findNumber(int[] arr, int number) {
-
-		int numberOfExecutedStatments = 0;
+	public int findNumber(int[] arr, int number, int numberOfExecutedStatment) {
 
 		for (int i = 0; i < arr.length; i++) {
 
-			numberOfExecutedStatments++;
+			numberOfExecutedStatment++;
 			if (arr[i] == number)
 				break;
 		}
-		return numberOfExecutedStatments;
+		return numberOfExecutedStatment;
+	}
+
+	/**
+	 * Logarithmic time complexity. O(log"n"), where "n" is the input in the
+	 * algorithm. The time complexity of this algorithm increases slowly, while the
+	 * input increases fast. the number of the executions of the statement "if (mid
+	 * == number)" is proportional to the logarithm of the size of "n".
+	 * 
+	 * Example, to find a number in a sorted array.
+	 * 
+	 * @param arr:    a sorted array of integers
+	 * @param number: the number to be found
+	 * @return: the number of the executions of the statement "if (mid == number)"
+	 */
+	public int findNumberLogarithmically(int[] arr, int number, int numberOfExecutedStatment) {
+
+		int mid = arr[arr.length / 2];
+		numberOfExecutedStatment++;
+		if (mid == number) {
+			return numberOfExecutedStatment;
+		}
+
+		if (number < mid) {
+
+			int[] firstHalfArray = new int[arr.length / 2];
+			for (int i = 0; i < arr.length / 2; i++) {
+				firstHalfArray[i] = arr[i];
+			}
+			return findNumberLogarithmically(firstHalfArray, number, numberOfExecutedStatment);
+
+		} else {
+			int[] secondHalfArray = new int[arr.length / 2];
+			for (int i = arr.length / 2; i < arr.length; i++) {
+				secondHalfArray[i - (arr.length / 2)] = arr[i];
+			}
+			return findNumberLogarithmically(secondHalfArray, number, numberOfExecutedStatment);
+		}
 	}
 }
